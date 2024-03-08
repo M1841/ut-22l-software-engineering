82ecbf968c61b6d621424f8021b110cc011bda78
M1841 <mihaimuresan592003@gmail.com>
2024-03-08 11:09:44 +0200

    Solved Exercise 2

--- isp-lab-2-2023-master/src/main/java/isp/lab2/Exercise2PrimeProduct/Exercise2PrimeProduct.java
+++ isp-lab-2-2023-master/src/main/java/isp/lab2/Exercise2PrimeProduct/Exercise2PrimeProduct.java
@@ -10,8 +10,27 @@ public class Exercise2PrimeProduct {
      * @param m
      * @return
      */
+
+    public static boolean isPrime(int x) {
+        if(x < 2) return false;
+        if(x == 2) return true;
+        if(x % 2 == 0) return false;
+
+        for(int i = 3; i * i < x; i++) {
+            if(x % i == 0) return false;
+        }
+
+        return true;
+    }
+
     public static long getPrimeProduct(int n, int m) {
-        return 0;
+        long product = 1;
+
+        for(int i = n; i <= m; i++) {
+            if(isPrime(i)) product *= i;
+        }
+
+        return product;
     }
 
 
@@ -19,7 +38,7 @@ public class Exercise2PrimeProduct {
      * This method should read from the console a number
      * @return the number read from the console
      */
-    public static int readfromConsoleInt() {
+    public static int readFromConsoleInt() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int number = scanner.nextInt();
@@ -28,7 +47,7 @@ public class Exercise2PrimeProduct {
 
     public static void main(String[] args) {
         System.out.println("The product of the first 10 prime numbers is: " + getPrimeProduct(1, 10));
-        System.out.println("The product of prime numbers between m and n: " + getPrimeProduct(readfromConsoleInt(), readfromConsoleInt()));
+        System.out.println("The product of prime numbers between m and n: " + getPrimeProduct(readFromConsoleInt(), readFromConsoleInt()));
     }
 
 }
