8c9775e8284f2cc85b5d46455c7167a033171f6d
M1841 <mihaimuresan592003@gmail.com>
2024-03-08 20:33:21 +0200

    Solved Exercise 5

--- isp-lab-2-2023-master/src/main/java/isp/lab2/Exercise5EvenOddSearch/Exercise5EvenOddSearch.java
+++ isp-lab-2-2023-master/src/main/java/isp/lab2/Exercise5EvenOddSearch/Exercise5EvenOddSearch.java
@@ -1,16 +1,50 @@
 package isp.lab2.Exercise5EvenOddSearch;
 
+import java.util.*;
+
 public class Exercise5EvenOddSearch {
 
     public static int[] findEvenOdd(String input) {
-        return null;
+
+        int[] nums = Arrays
+                .stream(input.split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[] max_min = {Integer.MIN_VALUE, -1, Integer.MAX_VALUE, -1};
+
+        for(int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if(max_min[0] < num && num % 2 == 0) {
+                max_min[0] = num;
+                max_min[1] = i;
+            }
+            if(max_min[2] > num && num % 2 != 0) {
+                max_min[2] = num;
+                max_min[3] = i;
+            }
+        }
+
+        return max_min;
+    }
+
+    public static String readFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numbers separated by commas: ");
+        return scanner.nextLine();
     }
 
     public static void main(String[] args) {
-        String input = "1,2,3,4,5,6,7,8,9,10";
+        String input = readFromConsole();
+
         int[] result = findEvenOdd(input);
-        for (int i : result) {
-            System.out.println(i);
-        }
+
+        if(result[1] == -1) System.out.println("The input contains no even numbers");
+        else System.out.println("Highest even number: " + result[0] + ", found at " +
+                "index: " + result[1]);
+        if(result[3] == -1) System.out.println("The input contains no odd numbers");
+        else System.out.println("Lowest odd number: " + result[2] + ", found at " +
+                "index: " + result[3]);
     }
 }
