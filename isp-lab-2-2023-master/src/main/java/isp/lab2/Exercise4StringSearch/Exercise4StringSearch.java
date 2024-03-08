24a3f182b7c6afe1bc52e91f66b1df7b14db338a
M1841 <mihaimuresan592003@gmail.com>
2024-03-08 11:45:19 +0200

    Solved Exercise 4

--- isp-lab-2-2023-master/src/main/java/isp/lab2/Exercise4StringSearch/Exercise4StringSearch.java
+++ isp-lab-2-2023-master/src/main/java/isp/lab2/Exercise4StringSearch/Exercise4StringSearch.java
@@ -1,29 +1,36 @@
 package isp.lab2.Exercise4StringSearch;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Exercise4StringSearch {
     /**
      * This method should return an array of strings that contain the substring.
      * @param input
-     * @param substring what to search for
+     * @param substr what to search for
      * @return
      */
-    public static String[] searchSubstrings(String input, String substring) {
+    public static String[] searchSubstrings(String input, String substr) {
         String[] splitInput = input.split(",");
+        List<String> result = new ArrayList<>();
 
-        return null;
+        for(String str: splitInput) {
+            if(str.contains(substr)) {
+                result.add(str);
+            }
+        }
+
+        return result.toArray(new String[0]);
     }
 
     public static String readFromConsole(String suffix) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter " + suffix);
+        System.out.println("Enter " + suffix + ": ");
         return scanner.nextLine();
     }
 
     public static void main(String[] args) {
-        String input = "apples,computer,bread,tea,car"; // = readFromConsole("words");
-        String substring = "te"; // = readFromConsole("substring");
+        String input = readFromConsole("words");
+        String substring = readFromConsole("substring");
         String[] result = searchSubstrings(input, substring);
         for (String string : result) {
             System.out.println(string);
